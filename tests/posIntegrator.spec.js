db8f8c8efa5a312da6d6676026e97977e82dd3db
Oli Gibson <oligibson1@gmail.com>
2016-01-03 14:42:24 +0000

    Added tests for Async Functions

--- tests/posIntegrator.spec.js
+++ tests/posIntegrator.spec.js
@@ -38,11 +38,6 @@ describe('Synchronous Messages to Gravity', function () {
 
     var pos, provider, router;
 
-    var items = [{
-        "itemID":"12345",
-        "microserviceReferenceID":"987654321"
-    }];
-
     // Get the provider
     beforeEach(module('pos.integrator', function ($posProvider,$routerProvider) {
         // This callback is only called during instantiation
@@ -63,6 +58,10 @@ describe('Synchronous Messages to Gravity', function () {
     });
 
     it('Add Item should send an message to the console in development mode', function(){
+        var items = [{
+            "itemID":"12345",
+            "microserviceReferenceID":"987654321"
+        }];
         pos.addItem(items);
         expect(console.info.calls.mostRecent().args[1].messageType).toEqual("AddItem");
         expect(console.info.calls.mostRecent().args[1].details.items.item).toEqual(items);
@@ -84,9 +83,9 @@ describe('Synchronous Messages to Gravity', function () {
 });
 
 
-describe('Get Scale Weights', function () {
+describe('pos.getWeight', function () {
 
-    var pos, provider, router, incomingMessageSpy, returnedMessage;
+    var pos, provider, router;
 
     // Get the provider
     beforeEach(module('pos.integrator', function ($posProvider,$routerProvider) {
@@ -99,211 +98,68 @@ describe('Get Scale Weights', function () {
     beforeEach(inject(function ($pos) {
         pos = $pos;
         provider.init('*', true);
+        console.info = spyOn(console, 'info')
     }));
 
-    beforeEach(function(done){
+    it('should send an message to the console in development mode and create a correlation id', function(){
         pos.getWeight(function(err, res){
-            if(err){
-                console.log(err);
-            }
-            returnedMessage = res;
+            if(err){console.log(err);}
+        });
+        expect(console.info.calls.mostRecent().args[1].messageType).toEqual("ReadScaleRequest");
+        expect(console.info.calls.mostRecent().args[1].correlationID).toBeDefined();
+    });
+
+
+    it('Should receive a response from POS Controller mock in development mode', function(){
+        pos.getWeight(function(err, res){
+            if(err){console.log(err);}
+            expect(res.messageType).toEqual("ReadScaleResponse");
+            expect(res.details.status).toEqual("success");
             done();
         });
     });
 
-
-
-    //beforeEach(function () {
-    //    incomingMessageSpy = spyOn(provider, 'routePOSCtrlMessage').and.callThrough();
-    //});
-
-    it('Should send a message to POS Controller and receive a response', function(){
-        expect(returnedMessage).toBeDefined();
-
-        //var message = {data: {messageType: 'ReadScaleResponse', correlationID: '987654321'}};
-        //// This needs to have a spec passed in!
-        //provider.routePOSCtrlMessage(message, router.spec);
-        //expect(incomingMessageSpy).toHaveBeenCalled();
-    })
-
-    //it('should call routePOSCtrlMessage', function () {
-    //    var message = {data: {messageType: 'ReadScaleResponse', correlationID: '123456789'}};
-    //    provider.routePOSCtrlMessage(message);
-    //    expect(incomingMessageSpy).toHaveBeenCalled();
-    //});
 });
 
 
-//describe('POS Integrator outbound functions', function () {
-//    var pos, outgoingSpy;
-//
-//    beforeEach(function(){
-//        module('pos.integrator');
-//    });
-//
-//    beforeEach(inject(function($pos){
-//        pos = $pos;
-//    }));
-//
-//    beforeEach(function(){
-//        outgoingSpy = spyOn(parent, 'postMessage');
-//    });
-//
-//    it('should send a dismiss message', function(){
-//        var message = {
-//            messageType: 'Dismiss'
-//        };
-//        pos.dismiss();
-//        expect(outgoingSpy).toHaveBeenCalledWith(message, '*');
-//    });
-//
-//    it('should send an add item message', function(){
-//        var message = {
-//            "messageType":"AddItem",
-//            "details": {
-//                "items": {
-//                    "item": [
-//                        {
-//                            "itemID":"12345",
-//                            "microserviceReferenceID":"987654321"
-//                        }
-//                    ]
-//                }
-//            }
-//        };
-//        pos.addItem(message.details.items.item);
-//        expect(outgoingSpy).toHaveBeenCalledWith(message, '*');
-//    });
-//
-//    it('should send a collect information response', function(){
-//        var message = {
-//            "messageType": " CollectInformationResponse",
-//            "correlationID": "123456789",
-//            "details": {
-//                "collectionType":"VATCustomerInfo",
-//                "status":"success",
-//                "customerReferenceID":"222333"
-//            }
-//        };
-//        pos.collectInfoResponse(message.correlationID, message.details);
-//        expect(outgoingSpy).toHaveBeenCalledWith(message, '*');
-//    });
-//
-//});
-//
-//describe('POS Integrator outbound functions', function () {
-//    var pos, outgoingSpy;
-//
-//    beforeEach(function(){
-//        module('pos.integrator');
-//    });
-//
-//    beforeEach(inject(function($pos){
-//        pos = $pos;
-//    }));
-//
-//    it('should send a dismiss message', function(){
-//        pos.test();
-//    });
-//
-//});
-//
-//describe('Print', function () {
-//
-//    var pos, provider, incomingMessageSpy;
-//
-//    // Get the provider
-//    beforeEach(module('pos.integrator', function ($posProvider) {
-//        // This callback is only called during instantiation
-//        provider = $posProvider;
-//    }));
-//
-//    // Kick off the above function
-//    beforeEach(inject(function ($pos) {
-//        pos = $pos;
-//    }));
-//
-//    beforeEach(function () {
-//        incomingMessageSpy = spyOn(provider, 'routePOSCtrlMessage').and.callThrough();
-//    });
-//
-//    it('Should send a message to POS Controller and receive a response', function(){
-//        pos.print("Receipt", function(err, res){
-//            if(err){
-//                console.log(err);
-//            }
-//            console.log(res);
-//            //expect(res).toBeDefined();
-//        });
-//
-//        //var message = {data: {messageType: 'ReadScaleResponse', correlationID: '987654321'}};
-//        //provider.routePOSCtrlMessage(message);
-//        //expect(incomingMessageSpy).toHaveBeenCalled();
-//    })
-//
-//    it('should call routePOSCtrlMessage', function () {
-//        var message = {data: {messageType: 'ReadScaleResponse', correlationID: '123456789'}};
-//        provider.routePOSCtrlMessage(message);
-//        expect(incomingMessageSpy).toHaveBeenCalled();
-//    });
-//});
-//
-//describe('Message', function () {
-//
-//    var spy, pos;
-//
-//    beforeEach(function(){
-//        module('pos.integrator');
-//    });
-//
-//    beforeEach(inject(function($pos){
-//        pos = $pos;
-//        pos.init();
-//    }));
-//
-//    //beforeEach(function(done) {
-//    //
-//    //    spy = spyOn(pos, 'routePOSCtrlMessage');
-//    //    spy();
-//    //    done();
-//    //
-//    //});
-//
-//    beforeEach(function(done) {
-//
-//        self.postMessage('test', '*');
-//        done();
-//
-//    });
-//
-//    it('Should trigger message event', function (done) {
-//        //expect(spy).toHaveBeenCalled();
-//        done();
-//    });
-//
-//
-//});
 
-//describe('Message', function () {
-//
-//    var spy;
-//
-//    beforeEach(function(done) {
-//
-//        spy = jasmine.createSpy('message');
-//
-//        window.addEventListener('message', function (e) {
-//            console.log(Object.keys(e), e.data);
-//            spy();
-//            done();
-//        });
-//
-//        self.postMessage('test', '*');
-//
-//    });
-//
-//    it('Should trigger message event', function () {
-//        expect(spy).toHaveBeenCalled();
-//    });
-//
-//});
+describe('pos.print', function () {
+
+    var pos, provider, router;
+    var tasks = [{
+        "Type":"printLine",
+        "Text":"Hello World 1"
+    }];
+
+    // Get the provider
+    beforeEach(module('pos.integrator', function ($posProvider,$routerProvider) {
+        // This callback is only called during instantiation
+        provider = $posProvider;
+        router = $routerProvider;
+    }));
+
+    // Kick off the above function
+    beforeEach(inject(function ($pos) {
+        pos = $pos;
+        provider.init('*', true);
+        console.info = spyOn(console, 'info')
+    }));
+
+    it('should send an message to the console in development mode and create a correlation id', function(){
+        pos.print("Receipt", true, false, tasks, function(err, res){
+            if(err){console.log(err);}
+        });
+        expect(console.info.calls.mostRecent().args[1].messageType).toEqual("PrintJobRequest");
+        expect(console.info.calls.mostRecent().args[1].correlationID).toBeDefined();
+    });
+
+
+    it('Should receive a response from POS Controller mock in development mode', function(){
+        pos.print("Receipt", true, false, tasks, function(err, res){
+            if(err){console.log(err);}
+            expect(res.messageType).toEqual("PrintJobResponse");
+            expect(res.details.status).toEqual("success");
+            done();
+        });
+    });
+});
