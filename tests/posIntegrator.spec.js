4cac05170a99832937c3788f5fb08346a673d4ce
Oli Gibson <oligibson1@gmail.com>
2016-01-03 13:51:42 +0000

    Added tests for Sync Messages in Dev Mode

--- tests/posIntegrator.spec.js
+++ tests/posIntegrator.spec.js
@@ -34,6 +34,56 @@ describe('Init', function () {
     //});
 });
 
+describe('Synchronous Messages to Gravity', function () {
+
+    var pos, provider, router;
+
+    var items = [{
+        "itemID":"12345",
+        "microserviceReferenceID":"987654321"
+    }];
+
+    // Get the provider
+    beforeEach(module('pos.integrator', function ($posProvider,$routerProvider) {
+        // This callback is only called during instantiation
+        provider = $posProvider;
+        router = $routerProvider;
+    }));
+
+    // Kick off the above function
+    beforeEach(inject(function ($pos) {
+        pos = $pos;
+        provider.init('*', true);
+        console.info = spyOn(console, 'info');
+    }));
+
+    it('Dismiss should send a message to the console in development mode', function(){
+        pos.dismiss();
+        expect(console.info.calls.mostRecent().args[1].messageType).toEqual("Dismiss");
+    });
+
+    it('Add Item should send an message to the console in development mode', function(){
+        pos.addItem(items);
+        expect(console.info.calls.mostRecent().args[1].messageType).toEqual("AddItem");
+        expect(console.info.calls.mostRecent().args[1].details.items.item).toEqual(items);
+    });
+
+    it('collectInfoResponse should send an message to the console in development mode', function(){
+        var id = "123456789";
+        var response = {
+            "collectionType":"VATCustomerInfo",
+            "status":"success",
+            "customerReferenceID":"222333"
+        };
+        pos.collectInfoResponse(id, response);
+        expect(console.info.calls.mostRecent().args[1].messageType).toEqual("CollectInformationResponse");
+        expect(console.info.calls.mostRecent().args[1].correlationID).toEqual(id);
+        expect(console.info.calls.mostRecent().args[1].details).toEqual(response);
+    });
+
+});
+
+
 describe('Get Scale Weights', function () {
 
     var pos, provider, router, incomingMessageSpy, returnedMessage;
@@ -82,6 +132,7 @@ describe('Get Scale Weights', function () {
     //    expect(incomingMessageSpy).toHaveBeenCalled();
     //});
 });
+
 
 //describe('POS Integrator outbound functions', function () {
 //    var pos, outgoingSpy;
